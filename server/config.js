9644a17339c1c71909df668c35ed2990e86762ad
Saurav Singh <53917524+snipextt@users.noreply.github.com>
2020-09-02 20:05:28 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,10 +1,3 @@
 module.exports = {
-    apiKey: "AIzaSyDtU6HZEj-YyQSPF34jlqoGb85MTbjEVrk",
-    authDomain: "social-media-app-cfd22.firebaseapp.com",
-    databaseURL: "https://social-media-app-cfd22.firebaseio.com",
-    projectId: "social-media-app-cfd22",
-    storageBucket: "social-media-app-cfd22.appspot.com",
-    messagingSenderId: "931224428553",
-    appId: "1:931224428553:web:fefdfd54e24307b2e798d8",
-    measurementId: "G-2CJ9F9SGVG"
+    //TODO: Add your own firebase config
   }
